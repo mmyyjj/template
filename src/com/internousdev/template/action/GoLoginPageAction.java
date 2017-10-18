3d901f17679896f82942ddc9281fb1559ab9b400
mmyyjj <mtmryuk01050607@docomo.ne.jp>
2017-10-18 13:49:53 +0900

    ログイン画面に遷移するためのアクションを追加。struts.xmlは先生のお手本と自分の作成した部分が混ざってしまわないようにスペースを開けました。

---
+++ src/com/internousdev/template/action/GoLoginPageAction.java
@@ -0,0 +1,20 @@
+package com.internousdev.template.action;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+/**
+ * ログイン画面へ遷移するためのアクション
+ * */
+public class GoLoginPageAction extends ActionSupport{
+
+	/**
+	 * 実行メソッド
+	 * @return reslut 結果
+	 * */
+	public String execute(){
+		String result = SUCCESS;
+		return result;
+
+	}
+
+}
