fb15aeb5f05184498c3cc7a2a303ce1b6d5139fe
mmyyjj <mtmryuk01050607@docomo.ne.jp>
2017-10-18 13:55:41 +0900

    GoLoginPageActionにjavadoc用のコメントを追記

--- src/com/internousdev/template/action/GoLoginPageAction.java
+++ src/com/internousdev/template/action/GoLoginPageAction.java
@@ -4,6 +4,9 @@ import com.opensymphony.xwork2.ActionSupport;
 
 /**
  * ログイン画面へ遷移するためのアクション
+ * @author YUKA MATSUMURA
+ * @since 2017/10/18
+ * @version 1.0
  * */
 public class GoLoginPageAction extends ActionSupport{
 
