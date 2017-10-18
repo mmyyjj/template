ad35cdda0ed50b87629adc97d86a472dec8678b0
mmyyjj <mtmryuk01050607@docomo.ne.jp>
2017-10-18 10:43:40 +0900

    ec_templete.sql追加

---
+++ src/sql/ec_templete.sql
@@ -0,0 +1,100 @@
+DROP DATABASE IF EXISTS ec_templete;
+
+CREATE DATABASE IF NOT EXISTS ec_templete;
+
+USE ec_templete;
+
+-- ユーザー情報テーブルの作成・データ挿入
+
+CREATE TABLE user_info_table(
+ user_id INT NOT NULL AUTO_INCREMENT,
+ user_name VARCHAR(20) NOT NULL,
+ mail_address VARCHAR(50) NOT NULL,
+ password VARCHAR(20) NOT NULL,
+ postal_code VARCHAR(10) NOT NULL,
+ street_address VARCHAR(200) NOT NULL,
+ phone_number VARCHAR(20) NOT NULL,
+ user_flg int(1) NOT NULL,
+ PRIMARY KEY(user_id),
+ UNIQUE INDEX(mail_address)
+);
+
+INSERT INTO user_info_table VALUES
+(0, 'テストユーザー1', 'test1@aaa', 'aaaa1111', '111-1111', '東京都○○区××1-11', '111-0000-0000', 0),
+(0, 'テストユーザー2', 'test2@bbb', 'bbbb2222', '222-2222', '東京都○○区××2-22', '222-0000-0000', 0),
+(0, 'テストユーザー3', 'test3@ccc', 'cccc3333', '333-3333', '東京都○○区××3-33', '333-0000-0000', 0),
+(0, '管理者1', 'admin1@aaa', 'aaaa1111', '111-1111', '東京都○○区××1-11-aa', '111-1111-0000', 1),
+(0, '管理者2', 'admin2@bbb', 'bbbb2222', '222-2222', '東京都○○区××2-22-bb', '222-2222-0000', 1),
+(0, '管理者3', 'admin3@ccc', 'cccc3333', '333-3333', '東京都○○区××3-33-cc', '333-3333-0000', 1);
+
+-- -----------------------------------------------------------------------------------------------
+
+
+-- 商品テーブルの作成・データ挿入
+
+CREATE TABLE product_table(
+ product_id INT NOT NULL AUTO_INCREMENT,
+ product_name VARCHAR(50) NOT NULL,
+ category VARCHAR(10) NOT NULL,
+ unit_price DECIMAL(10,0) NOT NULL,
+ current_stock INT NOT NULL,
+ number_of_sales INT NOT NULL,
+ comment VARCHAR(200) NOT NULL,
+ img_path VARCHAR(200) NOT NULL,
+ registration_date DATETIME NOT NULL,
+ update_date TIMESTAMP NOT NULL,
+ delete_flg INT(1) NOT NULL,
+ PRIMARY KEY(product_id),
+ UNIQUE INDEX(product_name)
+);
+
+INSERT INTO product_table
+ (product_id, product_name,category,unit_price,current_stock,number_of_sales,comment,img_path,registration_date,update_date,delete_flg)VALUES
+ (0, 'オリジナルマグカップ', 'キッチン用品', 1280, 50, 0, '当店のイメージキャラをあしらった限定マグカップ。<br>耐熱素材でレンジ加熱もOKです。', 'img/sample_1.png', NOW(), NOW(), 0),
+ (0, '止まり木ボールペン', 'ステーショナリー', 980, 50, 0, '止まり木モチーフのボールペンです。5ミリサイズ。' , 'img/sample_1.png', NOW(), NOW(), 0),
+ (0, '不苦労ミニだるま', 'ミニインテリア', 980, 50, 0, '幸運を呼ぶフクロウをモチーフにした小さなサイズのだるま。大きさ約5センチ。<br>玄関やリビングでそっとあなたを見守ります。', 'img/sample_1.png', NOW(), NOW(), 0);
+
+
+
+-- -----------------------------------------------------------------------------------------------
+
+
+-- カートテーブルの作成
+
+CREATE TABLE cart(
+ user_id INT NOT NULL,
+ product_id INT NOT NULL,
+ unit_price DECIMAL(10,0) NOT NULL,
+ order_number INT NOT NULL,
+ number_for_gift INT NOT NULL,
+ subtotal DECIMAL(10,0) NOT NULL,
+ INDEX(user_id)
+);
+
+
+-- -----------------------------------------------------------------------------------------------
+
+-- 注文履歴(概要)テーブルの作成
+
+CREATE TABLE history_outline_table(
+ order_id INT NOT NULL,
+ user_id INT NOT NULL,
+ total_price DECIMAL(10,0) NOT NULL,
+ order_date DATETIME NOT NULL,
+ delete_flg INT(1) NOT NULL,
+ PRIMARY KEY(order_id),
+ INDEX(user_id)
+);
+
+-- 注文履歴(詳細)テーブルの作成
+
+CREATE TABLE history_detail_table(
+ order_id INT NOT NULL,
+ product_id INT NOT NULL,
+ unit_price DECIMAL(10,0) NOT NULL,
+ order_number INT NOT NULL,
+ number_for_gift INT NOT NULL,
+ subtotal DECIMAL(10,0)
+);
+
+
