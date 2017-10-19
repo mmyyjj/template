ddf32c238f526108f91d0c1e04f71cdfc96196d1
mmyyjj <mtmryuk01050607@docomo.ne.jp>
2017-10-19 16:03:56 +0900

    DBConnectorクラスを編集。お手本の記述を写経、接続先を「ec_template」に変更しました。

--- src/com/internousdev/template/util/DBConnector.java
+++ src/com/internousdev/template/util/DBConnector.java
@@ -6,19 +6,25 @@ import java.sql.SQLException;
 
 public class DBConnector {
 	private static String driverName = "com.mysql.jdbc.Driver";
-	private static String url = "jdbc:mysql://localhost/ecsite";
+	private static String url = "jdbc:mysql://localhost/ec_template";
 	private static String user = "root";
 	private static String password = "mysql";
-public Connection getConnection() {
-	Connection con = null;
-	try {
-		Class.forName(driverName);
-		con = (Connection) DriverManager.getConnection(url,user,password);
-	} catch(ClassNotFoundException e) {
-		e.printStackTrace();
-	} catch(SQLException e) {
-		e.printStackTrace();
+
+	public Connection getConnection () {
+
+		Connection con = null;
+
+		try{
+			Class.forName(driverName);
+			con = (Connection) DriverManager.getConnection(url, user, password);
+		} catch(ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch(SQLException e) {
+			e.printStackTrace();
+		}
+
+		return con;
 	}
-	return con;
-	}
+
+
 }
