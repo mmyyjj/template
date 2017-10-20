913c50407d62f3969371a7448cf9ede9625ca98d
mmyyjj <mtmryuk01050607@docomo.ne.jp>
2017-10-20 13:57:55 +0900

    GoItemListPageActionを追加。中身はこれから書いていきます。

---
+++ src/com/internousdev/template/action/GoItemListPageAction.java
@@ -0,0 +1,12 @@
+package com.internousdev.template.action;
+
+/**
+ * 商品一覧ページを表示するためのアクションクラス。ユーザー側で使用。<br>
+ * DBから取得した商品のデータを格納し、jspに渡すためのリストを用意する。<br>
+ * ページネーションもここで行う。
+ * */
+public class GoItemListPageAction {
+
+
+
+}
