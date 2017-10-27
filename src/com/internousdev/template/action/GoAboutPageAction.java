d8bd6eee6c3d5e87e3ba0eb3cc885b970c0efbdf
mmyyjj <mtmryuk01050607@docomo.ne.jp>
2017-10-27 17:09:01 +0900

    GoAboutPageActionを編集。SessionAwareクラスを実装し忘れていたので、記述を追加しました。

--- src/com/internousdev/template/action/GoAboutPageAction.java
+++ src/com/internousdev/template/action/GoAboutPageAction.java
@@ -1,5 +1,10 @@
 package com.internousdev.template.action;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.struts2.interceptor.SessionAware;
+
 import com.opensymphony.xwork2.ActionSupport;
 
 /**
@@ -8,13 +13,34 @@ import com.opensymphony.xwork2.ActionSupport;
  * @since 2017/10/24
  * @version 1.0
  * */
-public class GoAboutPageAction extends ActionSupport{
+public class GoAboutPageAction extends ActionSupport implements SessionAware{
 
+	/**
+	 * セッション
+	 * */
+	private Map<String, Object> session = new HashMap<String, Object>();
 
+	/**
+	 * 実行メソッド
+	 * */
 	public String execute(){
 
 		return SUCCESS;
 
+	}
+
+	/**
+	 * @return session
+	 */
+	public Map<String, Object> getSession() {
+		return session;
+	}
+
+	/**
+	 * @param session セットする session
+	 */
+	public void setSession(Map<String, Object> session) {
+		this.session = session;
 	}
 
 
