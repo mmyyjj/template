334c4757037dc9619202d2f50582e94a2938b614
mmyyjj <mtmryuk01050607@docomo.ne.jp>
2017-10-30 13:36:38 +0900

    GoManagerProductListAction追記。executeの中身を記述しました。

--- src/com/internousdev/template/action/GoManagerProductListAction.java
+++ src/com/internousdev/template/action/GoManagerProductListAction.java
@@ -1,10 +1,15 @@
 package com.internousdev.template.action;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import org.apache.struts2.interceptor.SessionAware;
 
+import com.internousdev.template.dao.SelectItemDAO;
 import com.internousdev.template.dto.ProductInfoDTO;
+import com.internousdev.template.util.ItemListPaginator;
 import com.opensymphony.xwork2.ActionSupport;
 
 /**
@@ -16,19 +21,29 @@ import com.opensymphony.xwork2.ActionSupport;
 public class GoManagerProductListAction extends ActionSupport implements SessionAware{
 
 	/**
+	 * 検索ワード
+	 * */
+	private String select_word;
+
+	/**
+	 * 検索カテゴリ
+	 * */
+	private String select_category;
+
+	/**
 	 * 全商品のリスト
 	 * */
-	private ArrayList<ProductInfoDTO> allProductList = new ArrayList<ProductInfoDTO>();
+	private List<ProductInfoDTO> allProductList = new ArrayList<ProductInfoDTO>();
 
 	/**
 	 * ページネートされた商品リスト
 	 * */
-	private ArrayList<ProductInfoDTO> paginatedProductList = new ArrayList<ProductInfoDTO>();
+	private List<ProductInfoDTO> paginatedProductList = new ArrayList<ProductInfoDTO>();
 
 	/**
 	 * 現在のページ数
 	 * */
-	private int surrent_page;
+	private int current_page =1;
 
 	/**
 	 * 最大ページ数
@@ -36,53 +51,137 @@ public class GoManagerProductListAction extends ActionSupport implements Session
 	private int max_page;
 
 	/**
+	 * セッション
+	 * */
+	private Map<String, Object> session = new HashMap<String, Object>();
+
+
+
+
+	/**
 	 * 実行メソッド
 	 * */
 	public String execute(){
+		String result = ERROR;
+
+		SelectItemDAO sidao = new SelectItemDAO();
+		ItemListPaginator ilp = new ItemListPaginator();
+
+		allProductList = sidao.selectItem(select_word, select_category);
+		max_page = ilp.returnMaxPage(allProductList);
+
+		paginatedProductList = ilp.paginateItemList(allProductList, current_page);
+
+		result = SUCCESS;
+
+		return result;
+
+
 
 	}
+
+
+
+
+	/**
+	 * @return select_word
+	 */
+	public String getSelect_word() {
+		return select_word;
+	}
+
+
+
+
+	/**
+	 * @param select_word セットする select_word
+	 */
+	public void setSelect_word(String select_word) {
+		this.select_word = select_word;
+	}
+
+
+
+
+	/**
+	 * @return select_category
+	 */
+	public String getSelect_category() {
+		return select_category;
+	}
+
+
+
+
+	/**
+	 * @param select_category セットする select_category
+	 */
+	public void setSelect_category(String select_category) {
+		this.select_category = select_category;
+	}
+
+
+
 
 	/**
 	 * @return allProductList
 	 */
-	public ArrayList<ProductInfoDTO> getAllProductList() {
+	public List<ProductInfoDTO> getAllProductList() {
 		return allProductList;
 	}
+
+
+
 
 	/**
 	 * @param allProductList セットする allProductList
 	 */
-	public void setAllProductList(ArrayList<ProductInfoDTO> allProductList) {
+	public void setAllProductList(List<ProductInfoDTO> allProductList) {
 		this.allProductList = allProductList;
 	}
+
+
+
 
 	/**
 	 * @return paginatedProductList
 	 */
-	public ArrayList<ProductInfoDTO> getPaginatedProductList() {
+	public List<ProductInfoDTO> getPaginatedProductList() {
 		return paginatedProductList;
 	}
+
+
+
 
 	/**
 	 * @param paginatedProductList セットする paginatedProductList
 	 */
-	public void setPaginatedProductList(ArrayList<ProductInfoDTO> paginatedProductList) {
+	public void setPaginatedProductList(List<ProductInfoDTO> paginatedProductList) {
 		this.paginatedProductList = paginatedProductList;
 	}
 
-	/**
-	 * @return surrent_page
-	 */
-	public int getSurrent_page() {
-		return surrent_page;
-	}
+
+
 
 	/**
-	 * @param surrent_page セットする surrent_page
+	 * @return current_page
 	 */
-	public void setSurrent_page(int surrent_page) {
-		this.surrent_page = surrent_page;
+	public int getCurrent_page() {
+		return current_page;
 	}
+
+
+
+
+	/**
+	 * @param current_page セットする current_page
+	 */
+	public void setCurrent_page(int current_page) {
+		this.current_page = current_page;
+	}
+
+
+
 
 	/**
 	 * @return max_page
@@ -91,12 +190,37 @@ public class GoManagerProductListAction extends ActionSupport implements Session
 		return max_page;
 	}
 
+
+
+
 	/**
 	 * @param max_page セットする max_page
 	 */
 	public void setMax_page(int max_page) {
 		this.max_page = max_page;
 	}
+
+
+
+
+	/**
+	 * @return session
+	 */
+	public Map<String, Object> getSession() {
+		return session;
+	}
+
+
+
+
+	/**
+	 * @param session セットする session
+	 */
+	public void setSession(Map<String, Object> session) {
+		this.session = session;
+	}
+
+
 
 
 }
